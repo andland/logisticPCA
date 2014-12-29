9b060af7caaf4791d01433764272df0d9c019b30
Andrew Landgraf <andland@gmail.com>
2014-12-29 11:12:52 -0500

    tests classes and if response btwn 0 and 1

--- tests/testthat/test-dimensions.R
+++ tests/testthat/test-dimensions.R
@@ -13,64 +13,142 @@ lpca = logisticPCA(mat, M = 4, k = k, main_effects = FALSE)
 lsvd = logisticSVD(mat, k = k, main_effects = FALSE, max_iters = 50)
 
 pred1 = predict(lpca, mat)
-fit1a = fitted(lpca, type = "link")
-fit1b = fitted(lpca, type = "response")
+pred1l = predict(lpca, mat, type = "link")
+pred1r = predict(lpca, mat, type = "response")
+fit1l = fitted(lpca, type = "link")
+fit1r = fitted(lpca, type = "response")
 
 pred2 = predict(lsvd, mat)
-fit2a = fitted(lsvd, type = "link")
-fit2b = fitted(lsvd, type = "response")
+pred2l = predict(lsvd, mat, type = "link")
+pred2r = predict(lsvd, mat, type = "response")
+fit2l = fitted(lsvd, type = "link")
+fit2r = fitted(lsvd, type = "response")
+
+test_that("correct classes", {
+  expect_is(lpca, "lpca")
+  expect_is(lsvd, "lsvd")
+  
+  expect_is(pred1, "matrix")
+  expect_is(pred1l, "matrix")
+  expect_is(pred1r, "matrix")
+  expect_is(fit1l, "matrix")
+  expect_is(fit1r, "matrix")
+  
+  expect_is(pred2, "matrix")
+  expect_is(pred2l, "matrix")
+  expect_is(pred2r, "matrix")
+  expect_is(fit2l, "matrix")
+  expect_is(fit2r, "matrix")
+})
 
 test_that("k = 1 LPCA", {
-  expect_equal(dim(lpca$U), c(cols, k))
-  expect_equal(dim(lpca$PCs), c(rows, k))
+  expect_equal(dim(lpca$U), c(cols, 1))
+  expect_equal(dim(lpca$PCs), c(rows, 1))
   expect_equal(length(lpca$mu), cols)
   
-  expect_equal(dim(pred1), c(rows, k))
-  expect_equal(dim(fit1a), c(rows, cols))
-  expect_equal(dim(fit1b), c(rows, cols))
+  expect_equal(dim(pred1), c(rows, 1))
+  expect_equal(dim(pred1l), c(rows, cols))
+  expect_equal(dim(pred1r), c(rows, cols))
+  expect_equal(dim(fit1l), c(rows, cols))
+  expect_equal(dim(fit1r), c(rows, cols))
 })
 
 test_that("k = 1 LSVD", {
-  expect_equal(dim(lsvd$B), c(cols, k))
-  expect_equal(dim(lsvd$A), c(rows, k))
+  expect_equal(dim(lsvd$B), c(cols, 1))
+  expect_equal(dim(lsvd$A), c(rows, 1))
   expect_equal(length(lsvd$mu), cols)
   
-  expect_equal(dim(pred2), c(rows, k))
-  expect_equal(dim(fit2a), c(rows, cols))
-  expect_equal(dim(fit2b), c(rows, cols))
+  expect_equal(dim(pred2), c(rows, 1))
+  expect_equal(dim(pred2l), c(rows, cols))
+  expect_equal(dim(pred2r), c(rows, cols))
+  expect_equal(dim(fit2l), c(rows, cols))
+  expect_equal(dim(fit2r), c(rows, cols))
 })
 
 
-rm(lsvd, lpca, pred1, pred2, fit1a, fit1b, fit2a, fit2b)
+rm(lsvd, lpca, pred1, pred1l, pred1r, pred2, pred2l, pred2r, fit1l, fit1r, fit2l, fit2r)
 
 k = 2
 lpca = logisticPCA(mat, M = 4, k = k, main_effects = FALSE)
 lsvd = logisticSVD(mat, k = k, main_effects = FALSE, max_iters = 50)
 
 pred1 = predict(lpca, mat)
-fit1a = fitted(lpca, type = "link")
-fit1b = fitted(lpca, type = "response")
+pred1l = predict(lpca, mat, type = "link")
+pred1r = predict(lpca, mat, type = "response")
+fit1l = fitted(lpca, type = "link")
+fit1r = fitted(lpca, type = "response")
 
 pred2 = predict(lsvd, mat)
-fit2a = fitted(lsvd, type = "link")
-fit2b = fitted(lsvd, type = "response")
+pred2l = predict(lsvd, mat, type = "link")
+pred2r = predict(lsvd, mat, type = "response")
+fit2l = fitted(lsvd, type = "link")
+fit2r = fitted(lsvd, type = "response")
 
 test_that("k = 2 LPCA", {
-  expect_equal(dim(lpca$U), c(cols, k))
-  expect_equal(dim(lpca$PCs), c(rows, k))
+  expect_equal(dim(lpca$U), c(cols, 2))
+  expect_equal(dim(lpca$PCs), c(rows, 2))
   expect_equal(length(lpca$mu), cols)
   
-  expect_equal(dim(pred1), c(rows, k))
-  expect_equal(dim(fit1a), c(rows, cols))
-  expect_equal(dim(fit1b), c(rows, cols))
+  expect_equal(dim(pred1), c(rows, 2))
+  expect_equal(dim(pred1l), c(rows, cols))
+  expect_equal(dim(pred1r), c(rows, cols))
+  expect_equal(dim(fit1l), c(rows, cols))
+  expect_equal(dim(fit1r), c(rows, cols))
 })
 
 test_that("k = 2 LSVD", {
-  expect_equal(dim(lsvd$B), c(cols, k))
-  expect_equal(dim(lsvd$A), c(rows, k))
+  expect_equal(dim(lsvd$B), c(cols, 2))
+  expect_equal(dim(lsvd$A), c(rows, 2))
   expect_equal(length(lsvd$mu), cols)
   
-  expect_equal(dim(pred2), c(rows, k))
-  expect_equal(dim(fit2a), c(rows, cols))
-  expect_equal(dim(fit2b), c(rows, cols))
+  expect_equal(dim(pred2), c(rows, 2))
+  expect_equal(dim(pred2l), c(rows, cols))
+  expect_equal(dim(pred2r), c(rows, cols))
+  expect_equal(dim(fit2l), c(rows, cols))
+  expect_equal(dim(fit2r), c(rows, cols))
+})
+
+is_less_than_equal <- function (expected, label = NULL, ...) 
+{
+  if (is.null(label)) {
+    label <- testthat:::find_expr("expected")
+  }
+  else if (!is.character(label) || length(label) != 1) {
+    label <- deparse(label)
+  }
+  function(actual) {
+    diff <- expected - actual
+    expectation(diff >= 0, paste0("not less than ", label, 
+                                  ". Difference: ", format(diff)), paste0("is less than ", 
+                                                                          label))
+  }
+}
+
+is_more_than_equal <- function (expected, label = NULL, ...) 
+{
+  if (is.null(label)) {
+    label <- testthat:::find_expr("expected")
+  }
+  else if (!is.character(label) || length(label) != 1) {
+    label <- deparse(label)
+  }
+  function(actual) {
+    diff <- expected - actual
+    expectation(diff <= 0, paste0("not more than ", label, 
+                                  ". Difference: ", format(diff)), paste0("is more than"))
+  }
+}
+
+test_that("response between 0 and 1", {
+  expect_that(min(pred1r), is_more_than_equal(0))
+  expect_that(min(fit1r), is_more_than_equal(0))
+  
+  expect_that(max(pred1r), is_less_than_equal(1))
+  expect_that(max(fit1r), is_less_than_equal(1))
+  
+  expect_that(min(pred2r), is_more_than_equal(0))
+  expect_that(min(fit2r), is_more_than_equal(0))
+  
+  expect_that(max(pred2r), is_less_than_equal(1))
+  expect_that(max(fit2r), is_less_than_equal(1))
 })
